33e8b1f3a9ff9d7647b65d53992c68d50979a6ec
Romi kumar singh <romirajput544@gmail.com>
2024-02-26 10:28:50 +0530

    conditionals

---
+++ 02_conditionals/05_soln.py
@@ -0,0 +1,11 @@
+weather = "Sunny"
+
+if weather == "Sunny":
+    activity = "Go for a Walk"
+elif weather == "Rainy":
+    activity = "Read a book"
+elif weather == "Snowy":
+    activity = "Build a Snowman"
+
+print(activity)
+
